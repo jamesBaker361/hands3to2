47b90663c7ba3038615d47f87a6a2506097eca3f
James <jlbaker361@gmail.com>
2024-11-10 22:56:27 -0500

    new scene

--- screenshot_data.py
+++ screenshot_data.py
@@ -7,8 +7,19 @@ class SceneParameters:
         self.camera_locations_and_rotations=camera_locations_and_rotations #[(x,y,z,x,y,z)]
         self.light_locations_and_rotations=light_locations_and_rotations #[(x,y,z,x,y,z,power)]
 
-character_list=["budgie"]
+character_dict={"budgie":[math.pi/2,0,0],"emu":[math.pi/2,0,0]}
 scene_camera_params_dict={
     "dungeon":SceneParameters([0,-0.025,-.01,math.pi/2,0,math.pi/2],
                               [[0, 0, 0, math.pi *70/180, 0, math.pi*185/180]],
-                              [[.1, 0, 0, 0,0,0,5000]])}
+                              [[.1, 0, 0, 0,0,0,5000]]),
+      "Sofa":SceneParameters([-8,-5,3.2, 0,0,0],
+                             [[-19.355,-15.395,6.1234,math.pi/2,0,1.78*math.pi]],
+                             [[5, 5, 5, 0,0,0,5000]],
+                             5.0),
+        "room":SceneParameters([-0.5,-1,0.1,0,0,0],
+                               [[-3,1,2,.38*math.pi, 0,1.3*math.pi]],
+                               [[5, 5, 5, 0,0,0,5000]],
+                               0.25
+                               )                     
+                              
+    }
