e62bc38f29ce6252cb4b40efa9d6c9ea19750ee4
James Baker <jlbaker361@gmail.com>
2024-11-12 16:27:31 -0500

    fancy_office

--- screenshot_data.py
+++ screenshot_data.py
@@ -1,7 +1,7 @@
 import math
-
+from math import pi
 class SceneParameters:
-    def __init__(self,object_location_and_rotation:tuple, camera_locations_and_rotations:list,light_locations_and_rotations:list,object_scale:float=1.0) -> None:
+    def __init__(self,object_location_and_rotation:tuple, camera_locations_and_rotations:list,light_locations_and_rotations:list,object_scale:list=[1.0]) -> None:
         self.object_scale=object_scale
         self.object_location_and_rotation=object_location_and_rotation #x,y,z,x,y,z
         self.camera_locations_and_rotations=camera_locations_and_rotations #[(x,y,z,x,y,z)]
@@ -15,13 +15,13 @@ scene_camera_params_dict={
       "Sofa":SceneParameters([-8,-5,3.2, 0,0,0],
                              [[-19.355,-15.395,6.1234,math.pi/2,0,1.78*math.pi]],
                              [[5, 5, 5, 0,0,0,7.5]],
-                             5.0),
+                             [5.0]),
         "room":SceneParameters([-0.5,-1,0.1,0,0,0],
                                [[-3,1,2,.38*math.pi, 0,1.3*math.pi]],
                                [[5, 5, 5, 0,0,0,7.5],
                                 [5, 5, 5, 0,0,0,2.5]
                                 ],
-                               0.25
+                               [0.25]
                                ),           
         "room0":SceneParameters([-3.25,-1,0.7,0,0,0],
                                [[-4,1.65,1.7,0.44*math.pi, 0,1.05*math.pi],
@@ -30,16 +30,19 @@ scene_camera_params_dict={
                                [[5, 5, 5, 0,0,0,7.5],
                                 [5, 5, 5, 0,0,0,2.5]
                                 ],
-                               0.25
+                               [0.25]
                                ),
         "room1":SceneParameters([-5.2,-1,0.7,0,0,0],
-                               [[-4,1.65,1.7,0.44*math.pi, 0,1.05*math.pi],
-                                ],
+                                [[-3.2,-1,1.9,math.pi/3, 0,0.42*math.pi]],
                                [[5, 5, 5, 0,0,0,7.5],
-                                [5, 5, 5, 0,0,0,2.5]
-                                ],
-                               0.25
-                               )    
+                                [5, 5, 5, 0,0,0,2.5]],[0.25]),
+        "school":SceneParameters([-4,10.8,0.1,0,0,0],
+                                 [[-2,8,3,pi/3,0,pi/4]],
+                                 [[5, 5, 5, 0,0,0,7.5]],[0.5]),
+        "fancy_office":SceneParameters([2.2,3,0.745, 0,0,0],
+                                       [[5.07,1.4261,1.436, pi/2,0,0.39*pi]],
+                                       [[5, 5, 5, 0,0,0,7.5]],[0.75])
+
                               
     }
 
