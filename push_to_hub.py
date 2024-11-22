730692ae62d77f08d93cf43d5b40d6d5efd4a140
James Baker <jlbaker361@gmail.com>
2024-11-22 18:40:05 -0500

    error handling

--- push_to_hub.py
+++ push_to_hub.py
@@ -28,21 +28,24 @@ for csv_file in [f for f in os.listdir(metadata_dir) if f.endswith('.csv')]:
         
         # Read each row in the CSV
         for j,row in enumerate(csv_reader):
-            image=Image.open(row[0])
-            character=row[1]
-            x=row[2]
-            y=row[3]
-            x_1=row[4]
-            y_1=row[5]
-            angle=row[6]
-            map["image"].append(image)
-            map["character"].append(character)
-            map["x"].append(x)
-            map["y"].append(y)
-            map["x_1"].append(x_1)
-            map["y_1"].append(y_1)
-            map["angle"].append(angle)
+            try:
+                image=Image.open(row[0])
+                character=row[1]
+                x=row[2]
+                y=row[3]
+                x_1=row[4]
+                y_1=row[5]
+                angle=row[6]
+                map["image"].append(image)
+                map["character"].append(character)
+                map["x"].append(x)
+                map["y"].append(y)
+                map["x_1"].append(x_1)
+                map["y_1"].append(y_1)
+                map["angle"].append(angle)
 
-            if j%100==0 and j >100:
-                Dataset.from_dict(map).push_to_hub(repo_id)
+                if j%100==0 and j >100:
+                    Dataset.from_dict(map).push_to_hub(repo_id)
+            except:
+                pass
         Dataset.from_dict(map).push_to_hub(repo_id)
